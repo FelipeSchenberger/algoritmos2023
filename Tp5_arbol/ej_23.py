6dae170e7ed019a2d110026e935a882c6813e780
FelipeSchenberger <127799770+FelipeSchenberger@users.noreply.github.com>
2023-10-16 16:25:29 -0300

    ej_23.py

--- Tp5_arbol/ej_23.py
+++ Tp5_arbol/ej_23.py
@@ -39,36 +39,34 @@ else:
     print(buscado,'no se ha encontrado en la lista')
 print()
 
-# i
-#buscado = input('Ingrese la criatura que desea buscar: ')
-#criaturas.search_by_coincidence(buscado)
+ i
+buscado = input('Ingrese la criatura que desea buscar: ')
+criaturas.search_by_coincidence(buscado)
 
-# j
-#buscado = criaturas.search('Basilisco')
-#if buscado is not None:
-#    criaturas.delete_node('Basilisco')
-#buscado = criaturas.search('Sirenas')
-#if buscado is not None:
-#    criaturas.delete_node('Sirenas')
+ j
+buscado = criaturas.search('Basilisco')
+if buscado is not None:
+    criaturas.delete_node('Basilisco')
+buscado = criaturas.search('Sirenas')
+if buscado is not None:
+    criaturas.delete_node('Sirenas')
 
-#dic_ranking = {}
-#criaturas.inorden_ranking(dic_ranking)
-#
-#print(dic_ranking)
-#
-#
-#def order_por(item):
-#    print(item)
-#    return item[1]
-#
-#ordenados = list(dic_ranking.items())
-#ordenados.sort(key=order_por, reverse=True)
-#print(ordenados[:3])
-#
-#
-#pos = criaturas.search()
-#if pos is not None:
-#    pos.other_values['capturado'] = 'Heracles'
-#
+dic_ranking = {}
+criaturas.inorden_ranking(dic_ranking)
 
-#criaturas.search('tal')
+print(dic_ranking)
+
+
+def order_por(item):
+    print(item)
+    return item[1]
+
+ordenados = list(dic_ranking.items())
+ordenados.sort(key=order_por, reverse=True)
+print(ordenados[:3])
+
+pos = criaturas.search()
+if pos is not None:
+    pos.other_values['capturado'] = 'Heracles'
+
+criaturas.search('tal')
