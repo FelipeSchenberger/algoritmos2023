db141e72315374fc42c238352b2de556e0aabbd9
FelipeSchenberger <127799770+FelipeSchenberger@users.noreply.github.com>
2023-11-11 16:51:24 -0300

    ej_14.py

--- Tp_6_grafo/ej_14.py
+++ Tp_6_grafo/ej_14.py
@@ -1,56 +1,50 @@
 from grafo2 import Grafo
 from random import randint
 
-print ("\nEJERCICIO 14:")
+#14. Implementar sobre un grafo no dirigido los algoritmos necesario para dar solución a las siguientes tareas:
+#a. cada vértice representar un ambiente de una casa: cocina, comedor, cochera, quincho,
+#baño 1, baño 2, habitación 1, habitación 2, sala de estar, terraza, patio;
+#b. cargar al menos tres aristas a cada vértice, y a dos de estas cárguele cinco, el peso de la arista
+#es la distancia entre los ambientes, se debe cargar en metros;
+#c. obtener el árbol de expansión mínima y determine cuantos metros de cables se necesitan
+#para conectar todos los ambientes;
+#d. determinar cuál es el camino más corto desde la habitación 1 hasta la sala de estar para
+#determinar cuántos metros de cable de red se necesitan para conectar el router con el Smart Tv.
+
 grafo = Grafo(dirigido=False)
-print ("\nA")
-grafo.insert_vertice("banioUno")#
-grafo.insert_vertice("banioDos ")#
-grafo.insert_vertice("cochera")#
-grafo.insert_vertice("cocina")#
-grafo.insert_vertice("comedor")#
-grafo.insert_vertice("habitacionUno")#
-grafo.insert_vertice("habitacionDos")#
-grafo.insert_vertice("patio")#
-grafo.insert_vertice("salaDeEstar")#
-grafo.insert_vertice("terraza")#
-grafo.insert_vertice("quincho")#
-print ("Cargados")
+grafo.insert_vertice("banioUno")
+grafo.insert_vertice("banioDos ")
+grafo.insert_vertice("cochera")
+grafo.insert_vertice("cocina")
+grafo.insert_vertice("comedor")
+grafo.insert_vertice("habitacionUno")
+grafo.insert_vertice("habitacionDos")
+grafo.insert_vertice("patio")
+grafo.insert_vertice("salaDeEstar")
+grafo.insert_vertice("terraza")
+grafo.insert_vertice("quincho")
 
-print ("\nB:")
-grafo.insert_arist("banioUno","banioDos", randint(1, 30))#
-grafo.insert_arist("banioUno", "habitacionUno", randint(1, 15))#tres
+grafo.insert_arist("banioUno","banioDos", randint(1, 30))
+grafo.insert_arist("banioUno", "habitacionUno", randint(1, 15))
 grafo.insert_arist("banioUno", "salaDeEstar", randint(1, 20))
-
-grafo.insert_arist("banioDos", "habitacionDos", randint (1, 20))#dos + 1 que tiene con banioUno
+grafo.insert_arist("banioDos", "habitacionDos", randint (1, 20))
 grafo.insert_arist("banioDos","terraza", randint(1, 10))
-
-grafo.insert_arist("habitacionUno", "salaDeEstar", randint(1, 15)) #cuatro + 1 que tiene con banioUno
+grafo.insert_arist("habitacionUno", "salaDeEstar", randint(1, 15))
 grafo.insert_arist("habitacionUno", "habitacionDos", randint(1, 30))
 grafo.insert_arist("habitacionUno", "comedor", randint(1, 20))
 grafo.insert_arist("habitacionUno", "habitacionDos", randint(1, 30))
-
 grafo.insert_arist("habitacionDos", "terraza", randint (1, 15))
-grafo.insert_arist("habitacionDos", "quincho", randint (1, 35))#tres + 2 que tiene en (1 en habitacionuno, 1 en banioDos)
+grafo.insert_arist("habitacionDos", "quincho", randint (1, 35))
 grafo.insert_arist("habitacionDos", "patio", randint(1, 30))
-
-grafo.insert_arist("salaDeEstar", "patio", randint(1, 15))#uno + 2 que tiene en (1 en habitacionUno y 1 en banioUno)
-
-grafo.insert_arist("patio", "quincho", randint(1, 10))#uno + 2 que tiene en (1 en salabeEstar y 1 en habitacionDos)
-
-grafo.insert_arist ("quincho", "cochera", randint(1, 30))#uno + 2 que tiene en (1 en habitacionDos y 1 en patio)
-
-grafo.insert_arist("terraza", "cochera", randint(1, 30))#uno + 2 que tiene en (1 en banioDos y 1 en habitacionDos)
-
-grafo.insert_arist("cochera", "comedor", randint(1, 20))#uno + 2 que tiene en (1 en terraza, 1 en quincho)
-
+grafo.insert_arist("salaDeEstar", "patio", randint(1, 15))
+grafo.insert_arist("patio", "quincho", randint(1, 10))
+grafo.insert_arist("quincho", "cochera", randint(1, 30))
+grafo.insert_arist("terraza", "cochera", randint(1, 30))
+grafo.insert_arist("cochera", "comedor", randint(1, 20))
 grafo.insert_arist("cocina", "patio", randint(1, 20))
-grafo.insert_arist("cocina", "comedor", randint(1, 10))#tres
+grafo.insert_arist("cocina", "comedor", randint(1, 10))
 grafo.insert_arist("cocina", "salaDeEstar", randint (1, 20))
-#comedor ya tiene 3 (1 en cocina, 1 en cochera, 1 en habitacionUno)
 
-print ("Cargados")
-print ("\nC:")
 number = []
 for i in range(1, 31):
     number.append(str(i))
@@ -62,7 +56,6 @@ for arbol in grafo.kruskal():
 
 print(f"Se necesitaran {acu} metros.")
 
-print("\nD: ")
 ori = 'habitacionUno'
 des = 'salaDeEstar'
 origen = grafo.search_vertice(ori)
