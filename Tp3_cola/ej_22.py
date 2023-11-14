ec969ce68d675a2a49410ff064cf4fee3939fbae
FelipeSchenberger <127799770+FelipeSchenberger@users.noreply.github.com>
2023-11-14 02:30:48 -0300

    ej_22.py

--- Tp3_cola/ej_22.py
+++ Tp3_cola/ej_22.py
@@ -1,41 +1,101 @@
+from cola1 import Cola
+        
+'''22. Se tienen una cola con personajes de Marvel Cinematic Universe (MCU), de los cuales se conoce
+el nombre del personaje, el nombre del superhéroe y su género (Masculino M y Femenino
+F)por ejemplo {Tony Stark, Iron Man, M}, {Steve Rogers, Capitán América, M}, {Natasha Romanoff,
+Black Widow, F}, etc., desarrollar un algoritmo que resuelva las siguientes actividades:
+a. determinar el nombre del personaje de la superhéroe Capitana Marvel;
+b. mostrar los nombre de los superhéroes femeninos;
+c. mostrar los nombres de los personajes masculinos;
+d. determinar el nombre del superhéroe del personaje Scott Lang;
+e. mostrar todos datos de los superhéroes o personaje cuyos nombres comienzan con la letra S;
+f. determinar si el personaje Carol Danvers se encuentra en la cola e indicar su nombre
+de superhéroes.'''
+
 personajes = [
     {'nombre':'Steve Rogers','superheroe':'Capitan America','sexo':'M'},
     {'nombre':'Peter Parker','superheroe':'Spider-Man','sexo':'M'},
     {'nombre':'Kate Bishop','superheroe':'Ojo de halcon','sexo':'F'},
     {'nombre':'Carol Danvers','superheroe':'Capitana Marvel','sexo':'F'},
-    {'nombre':'Stephen Strange','superheroe':'Doctor Strange','sexo':'M'},
+    {'nombre':'stephen Strange','superheroe':'Doctor Strange','sexo':'M'},
     {'nombre':'Scott Lang','superheroe':'Ant-Man','sexo':'M'}
 ]
-nombres_sexo_f = []
-nombres_sexo_m = []
-empiezan_con_s = []
+
+cola_personajes = Cola()
+
+#a
+nombre_capitana_marvel = None
+
+for personaje in personajes:
+    cola_personajes.arrive(personaje)
+    if personaje['superheroe'] == 'Capitana Marvel':
+        nombre_capitana_marvel = personaje['nombre']
+        break
+
+if nombre_capitana_marvel:
+    print(f"El nombre del personaje asociado a 'Capitana Marvel' es: {nombre_capitana_marvel}")
 
 
-for persona in personajes:
-    if persona['superheroe'] == 'Capitana Marvel':
-        nombre = persona['nombre']
-        
-    if persona['sexo'] == 'F':
-        nombre_sexo_f = persona['nombre']
-        nombres_sexo_f.append(nombre_sexo_f)
-    
-    if persona['sexo'] == 'M':
-        nombre_sexo_m = persona['nombre']
-        nombres_sexo_m.append(nombre_sexo_m)
-    
-    if persona['nombre'] == 'Scott Lang':
-        superheroe = persona['superheroe']
+#b
+for personaje in personajes:
+    cola_personajes.arrive(personaje)
 
-    if persona['nombre'].startswith('S') or persona['superheroe'].startswith('S'):
-        empieza_con_s = persona['nombre'],persona['superheroe'],persona['sexo']
-        empiezan_con_s.append(empieza_con_s)
+print("Nombres de superhéroes femeninos:")
+while cola_personajes.size() > 0:
+    personaje_actual = cola_personajes.atention()
+    if personaje_actual['sexo'] == 'F':
+        print(personaje_actual['nombre'])
 
-    if persona['nombre'] == 'Carol Danvers':
-        carol = persona['superheroe']
+#c
+for personaje in personajes:
+    cola_personajes.arrive(personaje)
 
-print(f'a) {nombre}')
-print(f'b) {nombres_sexo_f}')
-print(f'c) {nombres_sexo_m}')
-print(f'd) {superheroe}')
-print(f'e) {empiezan_con_s}')
-print(f'f) Carol Danvers se encuentra en la cola y su nombre de superheroe es {carol}')
+print("Nombres de superhéroes masculinos:")
+while cola_personajes.size() > 0:
+    personaje_actual = cola_personajes.atention()
+    if personaje_actual['sexo'] == 'M':
+        print(personaje_actual['nombre'])
+
+#d
+nombre_scott_lang = None
+
+for personaje in personajes:
+    cola_personajes.arrive(personaje)
+    if personaje['nombre'] == 'Scott Lang':
+        nombre_scott_lang = personaje['superheroe']
+        break
+
+if nombre_scott_lang:
+    print(f"El nombre de superheroe asociado a 'Scott Lang' es: {nombre_scott_lang}")
+
+#e
+for personaje in personajes:
+    cola_personajes.arrive(personaje)
+
+print("Superhéroes con la letra 'S':")
+while cola_personajes.size() > 0:
+    personaje_actual = cola_personajes.atention()
+    if personaje_actual['nombre'][0].upper() == 'S':
+        print(personaje_actual)
+
+#f
+nombre_a_buscar = input("Ingrese el nombre del personaje a buscar: ")
+
+personaje_encontrado = None
+cola_temporal = Cola()
+
+while cola_personajes.size() > 0:
+    personaje_actual = cola_personajes.atention()
+    if personaje_actual['nombre'] == nombre_a_buscar:
+        personaje_encontrado = personaje_actual
+        break
+    else:
+        cola_temporal.arrive(personaje_actual)
+
+while cola_temporal.size() > 0:
+    cola_personajes.arrive(cola_temporal.atention())
+
+if personaje_encontrado:
+    print(f"El personaje {nombre_a_buscar} se encuentra en la cola. Su nombre de superhéroe es: {personaje_encontrado['superheroe']}")
+else:
+    print(f"No se encontró un personaje con el nombre {nombre_a_buscar} en la cola.")
